30fc904e1ece0b9d7bf930e362b6d3d4d87dc68f
David L. Inman <davidlevere@gmail.com>
2020-11-04 23:50:19 -0500

    struct def / comment list

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+
+/*
+    expect
+    parse_symbol
+    parse_number
+    parse_string
+    maybe
+    zero_or_more
+    one_or_more
+    list
+    choice
+*/
+
+#[derive(Debug)]
+pub enum Parser {
+    Expect(String),
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
